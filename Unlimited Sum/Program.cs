034473ed82c83a901c0f664c869496758020747b
Muhammad Ali <m.aleydev@gmail.com>
2022-02-06 14:03:56 +0500

    Add files via upload

---
+++ Unlimited Sum/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+
+namespace Unlimited_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList myArray = new ArrayList();
+            bool check = true;
+            int sum = 0;
+            while (check)
+            {
+                Console.Write("Number or Command? ");
+                var inp =Console.ReadLine();
+                if (inp == "sum")
+                {
+                    Console.WriteLine("Sum is: "+ sum);
+                }
+                else if (inp == "view")
+                {
+                    foreach (var item in myArray)
+                    {
+                        
+                            Console.WriteLine("element: " + item);
+                    }
+                }
+                else if (inp == "end")
+                {
+                    check = false;
+                }
+                else
+                {
+                    myArray.Add(inp);
+                    sum += int.Parse(inp);
+                }
+            }
+
+
+        }
+    }
+}
